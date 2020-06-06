a9f8286ddcb6b4d45ced0df4710a7e4ca841e66c
Nova Hairiyanov <mhairiyanov@gmail.com>
2020-06-06 14:26:15 +0700

    init commit

---
+++ app/src/main/java/com/NPM/praktek_ke_1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.NPM.praktek_ke_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
